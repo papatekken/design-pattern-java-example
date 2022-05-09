f1953874243b81a1e0ffb8f92c5ed97201f3157f
Allan Yip <papatekken@gmail.com>
2022-05-09 17:57:30 +0800

    observer add mission

--- observer/example1/src/main/java/Mission.java
+++ observer/example1/src/main/java/Mission.java
@@ -1,2 +1,9 @@
 public class Mission {
+    private  String name;
+    private String objective;
+
+    public Mission(String name,String objective){
+        this.name = name;
+        this.objective = objective;
+    }
 }
