1ff9182da8d75f46a7159caac0ecbf9c537abd5f
Allan Yip <papatekken@gmail.com>
2022-05-13 07:47:51 +0800

    add main class

--- observer/example1/src/main/java/SecretOrganization.java
+++ observer/example1/src/main/java/SecretOrganization.java
@@ -1,13 +1,18 @@
-import java.util.LinkedList;
+import java.util.HashSet;
+
 
 public class SecretOrganization {
     public String name  ="";
-    public LinkedList<Spy> agents;
+    public HashSet<Spy> agents;
     public SecretOrganization(String name){
         this.name =name;
     }
 
-    public void join(Spy agent){}
+    public void join(Spy agent){
+        agents.add(agent);
+    }
+
+
     public void terminate(Spy agent){}
     public Mission createMission(){ return null;}
     public void sendMission(){}
