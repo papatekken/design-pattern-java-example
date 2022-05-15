f50aafd8c3f1926d4ed1df821de2b7720d7d343a
Allan Yip <papatekken@gmail.com>
2022-05-15 19:27:53 +0800

    observer start to build main function

--- observer/example1/src/main/java/Main.java
+++ observer/example1/src/main/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
-
     public static void main(String[] args){
             SecretOrganization kingsman = new SecretOrganization("Kingsman");
-
+            Spy galahad = new Spy("Galahad");
+            Spy percival = new Spy("Percival");
+            Spy lancelot = new Spy("Lancelot");
+            kingsman.join( galahad);
+            kingsman.join( percival);
+            kingsman.join( lancelot);
     }
 }
